a1e6b6a736c726544166f884150862070ca36dfc
Jesús Mª Rico González <jrico@narranz.es>
2021-02-23 16:42:09 +0100

    Añadiendo middleware de errores a solicitudes y rutas

--- server/index.js
+++ server/index.js
@@ -6,8 +6,20 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', ( req, res )=>{
-    res.send('hola mundo')
+
+// ROUTE GET - INDEX
+app.get('/', ( req, res ,next)=>{
+   res.status(200).json({
+       msg:'Solicitud GET / '
+   })
+})
+
+// Error handler - No existe o 404
+app.use((err, req, res, next) => {
+    const { statusCode = 500 , message } = err
+    res.status( statusCode ).json({
+        message,
+    })
 })
 
 module.exports = app
