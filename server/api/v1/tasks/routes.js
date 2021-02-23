2dcc04fd5c265bc194a44c4eca9ea8d600386949
Jesús Mª Rico González <jrico@narranz.es>
2021-02-23 22:47:16 +0100

    Creando API layout para Tasks

--- server/api/v1/tasks/routes.js
+++ server/api/v1/tasks/routes.js
@@ -6,11 +6,11 @@ const router = require('express').Router()
 const controller = require('./controller');
 
 /*
-* /api/tasks/ POST - CREATE
-* /api/tasks/ GET - READ ALL
-* /api/tasks/:id GET - READ ONE
-* /api/tasks/:id PUT - UPDATE
-* api/tasks/:id DELETE - DELETE
+* /api/tasks/    POST   - CREATE
+* /api/tasks/    GET    - READ ALL
+* /api/tasks/:id GET    - READ ONE
+* /api/tasks/:id PUT    - UPDATE
+* /api/tasks/:id DELETE - DELETE
 */
 
 router
