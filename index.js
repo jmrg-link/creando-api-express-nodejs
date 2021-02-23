9cebb47fa0dd85c89688c19e2ad8127dddfcd25b
Jesús Mª Rico González <jrico@narranz.es>
2021-02-23 15:24:36 +0100

    Utilizando http como webserver

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+
+const http = require ('http')
+
+const hostname = '127.0.0.1'
+const port = 8080
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hola Mundo')
+});
+
+server.listen(port,hostname,() =>{
+    console.log(`Servidor corriendo en htto://${hostname}:${port}`)
+})
